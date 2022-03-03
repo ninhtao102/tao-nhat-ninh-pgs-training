be3d9b5a157dd033668f9bfe7f1affa5b958e286
ninhtao102 <ninh.tao.102@gmail.com>
2022-03-03 21:54:42 +0700

    create modal

---
+++ src/modules/product/components/AddProductForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {}
+
+const AddProductForm = (props: Props) => {
+  return <div>AddProductForm</div>;
+};
+
+export default AddProductForm;
---
+++ src/modules/product/pages/AddProductPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import AddProductForm from '../components/AddProductForm';
+
+interface Props {}
+
+const AddProductPage = (props: Props) => {
+  return (
+    <div>
+      <AddProductForm />
+    </div>
+  );
+};
+
+export default AddProductPage;
