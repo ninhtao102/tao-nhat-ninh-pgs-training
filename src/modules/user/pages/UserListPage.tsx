40f20412d8442e9e57f1e9c6caa99313563644ac
ninhtao102 <ninh.tao.102@gmail.com>
2022-03-24 21:03:33 +0700

    add fetchData and handler

--- src/modules/user/pages/UserListPage.tsx
+++ src/modules/user/pages/UserListPage.tsx
@@ -1,17 +1,73 @@
 import { Box, Button, Link } from '@mui/material';
-import React from 'react';
-import { Control } from 'react-hook-form';
+import React, { useCallback, useEffect, useState } from 'react';
+import { API_HEADER, API_PATHS } from '../../../configs/api';
 import { IUserFilter } from '../../../models/filter';
+import { IUserItem } from '../../../models/user';
+import { ISort } from '../../../models/utils';
 import UserFilter from '../components/filter/UserFilter';
 import UserFooter from '../components/table/UserFooter';
 import UserListTable from '../components/table/UserListTable';
 
-interface Props {
-  control: Control<IUserFilter, any>;
-}
+interface Props {}
 
 const UserListPage = (props: Props) => {
-  const { control } = props;
+  const [filter, setFilter] = useState<IUserFilter>();
+  const [sortInfo, setsortInfo] = useState<ISort>({
+    order_by: 'name',
+    sort: 'asc',
+  });
+  const [totalItem, setTotalItem] = useState<number>(0);
+  const [tableData, setTableData] = useState<IUserItem[]>();
+  const [pageInfo, setPageInfo] = useState(1);
+  const [checked, setChecked] = useState(false);
+
+  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null, number: number) => {
+    setPageInfo(number);
+  };
+
+  const handleFilter = (data: IUserFilter) => {
+    setFilter(data);
+  };
+
+  const handleCheckItem = (checked: any) => {
+    setChecked(!checked);
+    // tableData[index]
+  };
+
+  const handleSort = (id: string) => {
+    const isSort = sortInfo?.order_by === id && sortInfo.sort === 'desc';
+    setsortInfo({ sort: isSort ? 'asc' : 'desc', order_by: id });
+  };
+
+  const fetchData = useCallback(() => {
+    fetch(API_PATHS.users, {
+      method: 'post',
+      ...API_HEADER,
+      body: JSON.stringify({
+        ...filter,
+        order_by: sortInfo?.sort.toUpperCase(),
+        page: pageInfo,
+        sort: sortInfo?.order_by,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log('tableData', data);
+        // console.log('recordsTotal', data.recordsTotal);
+        const newData = data.data.map((item: any) => {
+          return { ...item, checked: false };
+        });
+        setTableData(newData);
+        setTotalItem(data.recordsTotal);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, [filter, pageInfo, sortInfo]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
   return (
     <>
@@ -22,7 +78,7 @@ const UserListPage = (props: Props) => {
           padding: '5vh',
         }}
       >
-        <UserFilter control={control} />
+        <UserFilter handleFilter={handleFilter} />
         <Button
           variant="contained"
           sx={{
@@ -41,8 +97,19 @@ const UserListPage = (props: Props) => {
           <Link id="newProductLink" href="http://localhost:3000/user/new-user" underline="none" sx={{ color: '#fff' }}>
             Add User
           </Link>
+          {/* Link react dom */}
         </Button>
-        <UserListTable />
+        {tableData && (
+          <UserListTable
+            tableData={tableData}
+            sortInfo={sortInfo}
+            totalItem={totalItem}
+            pageInfo={pageInfo}
+            handleSort={handleSort}
+            handleCheckItem={handleCheckItem}
+            handleChangePage={handleChangePage}
+          />
+        )}
         <Box sx={{ position: 'fixed', bottom: '3px', width: 'calc(100vw - 22vw)' }}>
           <UserFooter />
         </Box>
