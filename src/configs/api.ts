0dcb6952e231adfacb6febd22044274129139816
ninhtao102 <ninh.tao.102@gmail.com>
2022-03-26 11:51:29 +0700

    Modified api.ts

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -21,11 +21,16 @@ export const API_PATHS = {
   signIn: `${getBaseUrl(APIService.public)}/authentication/login`,
   products: `${getBaseUrl(APIService.public)}/products/list`,
   categories: `${getBaseUrl(APIService.public)}/categories/list`,
+  uploadImage: `${getBaseUrl(APIService.public)}/products/upload-image`,
   brands: `${getBaseUrl(APIService.auth)}/brands/list`,
   users: `${getBaseUrl(APIService.auth)}/users/list`,
   role: `${getBaseUrl(APIService.auth)}/commons/role`,
   country: `${getBaseUrl(APIService.auth)}/commons/country`,
   shipping: `${getBaseUrl(APIService.auth)}/shipping/list`,
+  vendors: `${getBaseUrl(APIService.auth)}/vendors/list`,
+  productEdit: `${getBaseUrl(APIService.auth)}/products/edit`,
+  productCreate: `${getBaseUrl(APIService.auth)}/products/create`,
+  productDetail: `${getBaseUrl(APIService.auth)}/products/detail`,
 };
 
 export const API_HEADER = {
