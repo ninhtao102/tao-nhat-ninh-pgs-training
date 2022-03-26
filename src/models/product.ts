1e3a4006dc283225f765bb18276785d00c7a5bde
ninhtao102 <ninh.tao.102@gmail.com>
2022-03-26 11:52:41 +0700

    Add IProductDetail

--- src/models/product.ts
+++ src/models/product.ts
@@ -1,23 +1,20 @@
-export interface IProductFilter {
-  searchKey: string;
-  category: string;
-  stockStatus: string;
-  searchIn: string[];
-  availability: string;
-  vendor: string;
-}
-
 export interface IProductItem {
-  id: string;
-  sku: string;
-  name: string;
-  description: string;
-  category: string;
-  price: string;
-  amount: number;
-  vendor: string;
+  amount: string;
   arrivalDate: string;
-  checked?: boolean;
+  category: string;
+  condition: string;
+  created: string;
+  description: string;
+  enabled: string;
+  id: string;
+  name: string;
+  participateSale: string;
+  price: string;
+  sku: string;
+  vendor: string;
+  vendorID: string;
+  weight: string;
+  checked: boolean;
 }
 
 export interface IProductParams {
@@ -42,4 +39,39 @@ export interface IProductParams {
   metaDesc: string;
   metaKeywords: string;
   productPageTitle: string;
+}
+
+export interface IProductDetail {
+  arrival_date: string;
+  brand_id: string;
+  categories: [];
+  cleanURL: string;
+  code: string;
+  condition_id: string;
+  description: string;
+  enabled: string;
+  facebook_marketing_enabled: string;
+  google_feed_enabled: string;
+  id: string;
+  images: [];
+  inventory_tracking: string;
+  memberships: [];
+  meta_desc_type: string;
+  meta_description: string;
+  meta_keywords: string;
+  name: string;
+  og_tags: string;
+  og_tags_type: string;
+  participate_sale: string;
+  price: string;
+  product_page_title: string;
+  quantity: string;
+  sale_price: string;
+  sale_price_type: string;
+  shipping: [];
+  sku: string;
+  sort_description: string;
+  tax_exempt: string;
+  vendor_id: string;
+  weight: string;
 }
