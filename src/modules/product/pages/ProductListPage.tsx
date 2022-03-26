08190af807aec28dbca5b9dc9feb5f581dbb81d0
ninhtao102 <ninh.tao.102@gmail.com>
2022-03-26 11:57:26 +0700

    Modified and add logic

--- src/modules/product/pages/ProductListPage.tsx
+++ src/modules/product/pages/ProductListPage.tsx
@@ -1,15 +1,73 @@
 import { Box, Button } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { API_HEADER, API_PATHS } from '../../../configs/api';
+import { IProductFilter } from '../../../models/filter';
+import { IProductItem } from '../../../models/product';
+import { ISort } from '../../../models/utils';
 import ProductFilter from '../components/filter/ProductFilter';
 import ProductListFooter from '../components/table/ProductListFooter';
 import ProductListTable from '../components/table/ProductListTable';
-import { IProductFilter } from '../../../models/product';
-import { Link } from 'react-router-dom';
 
 interface Props {}
 
 const ProductListPage = (props: Props) => {
-  const [filterValue, setFilterValue] = useState<IProductFilter>();
+  const [filter, setFilter] = useState<IProductFilter>();
+  const [sortInfo, setsortInfo] = useState<ISort>({
+    order_by: 'name',
+    sort: 'asc',
+  });
+  const [totalItem, setTotalItem] = useState();
+  const [tableData, setTableData] = useState<IProductItem[]>();
+  const [pageInfo, setPageInfo] = useState(1);
+  const [checked, setChecked] = useState(false);
+
+  const handleChangePage = (event: React.MouseEvent<HTMLButtonElement, MouseEvent> | null, number: number) => {
+    setPageInfo(number);
+  };
+
+  const handleFilter = (data: IProductFilter) => {
+    setFilter(data);
+  };
+
+  const handleCheckItem = (checked: any) => {
+    setChecked(!checked);
+    // tableData[index]
+  };
+
+  const handleSort = (id: string) => {
+    const isSort = sortInfo?.order_by === id && sortInfo.sort === 'desc';
+    setsortInfo({ sort: isSort ? 'asc' : 'desc', order_by: id });
+  };
+
+  const fetchData = useCallback(() => {
+    fetch(API_PATHS.products, {
+      method: 'post',
+      ...API_HEADER,
+      body: JSON.stringify({
+        ...filter,
+        order_by: sortInfo?.sort.toUpperCase(),
+        page: pageInfo,
+        sort: sortInfo?.order_by,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const newData = data.data.map((item: any) => {
+          return { ...item, checked: false };
+        });
+        setTableData(newData);
+        setTotalItem(data.recordsTotal);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, [filter, pageInfo, sortInfo]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <>
       <Box
@@ -19,8 +77,8 @@ const ProductListPage = (props: Props) => {
           padding: '5vh',
         }}
       >
-        <ProductFilter />
-        <Link style={{ textDecoration: 'none' }} to="/products/manage-product">
+        <ProductFilter handleFilter={handleFilter} />
+        <Link style={{ textDecoration: 'none' }} to="/products/new-product">
           <Button
             variant="contained"
             sx={{
@@ -39,7 +97,15 @@ const ProductListPage = (props: Props) => {
             Add Product
           </Button>
         </Link>
-        <ProductListTable />
+        <ProductListTable
+          tableData={tableData}
+          sortInfo={sortInfo}
+          totalItem={totalItem}
+          pageInfo={pageInfo}
+          handleSort={handleSort}
+          handleCheckItem={handleCheckItem}
+          handleChangePage={handleChangePage}
+        />
         <Box sx={{ position: 'fixed', bottom: '3px', width: 'calc(100vw - 22vw)' }}>
           <ProductListFooter />
         </Box>
