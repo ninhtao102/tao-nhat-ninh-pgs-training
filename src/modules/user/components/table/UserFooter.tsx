883fba4b863ccef436ac5e8b55c6391148f402ca
ninhtao102 <ninh.tao.102@gmail.com>
2022-04-01 04:24:56 +0700

    Add modal confirm delete

--- src/modules/user/components/table/UserFooter.tsx
+++ src/modules/user/components/table/UserFooter.tsx
@@ -1,9 +1,15 @@
-import { Box, Button, Grid } from '@mui/material';
+import { Box, Button, Modal, Typography } from '@mui/material';
 import React from 'react';
 
-interface Props {}
+interface Props {
+  btnDisable: boolean;
+  handleRemovebtn(): void;
+}
 
 const UserFooter = (props: Props) => {
+  const { btnDisable, handleRemovebtn } = props;
+  const [isModalOpen, setIsModalOpen] = React.useState(false);
+
   return (
     <>
       <Box
@@ -14,28 +20,60 @@ const UserFooter = (props: Props) => {
           boxShadow: '1px 1px 11px #b18aff',
         }}
       >
-        <Grid container spacing={2}>
-          <Grid item xs={5}>
-            <Box>
-              <Button
-                variant="contained"
-                sx={{
-                  marginRight: '2vh',
-                  opacity: '0.5',
-                  backgroundColor: '#f0ad4e',
-                  '&: hover': {
-                    backgroundColor: '#f0ad4e',
-                    color: '#000',
-                  },
-                }}
-              >
-                Remove selected
-              </Button>
-            </Box>
-          </Grid>
-          <Grid item xs={7}></Grid>
-        </Grid>
+        <Box>
+          <Button
+            variant="contained"
+            disabled={btnDisable}
+            sx={{
+              width: '210px',
+              backgroundColor: '#f0ad4e',
+              '&: hover': {
+                backgroundColor: '#f0ad4e',
+                color: '#000',
+              },
+            }}
+            onClick={() => setIsModalOpen(true)}
+          >
+            Remove selected
+          </Button>
+        </Box>
       </Box>
+      <Modal
+        open={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box className="modal_content" style={{ backgroundColor: '#323259' }}>
+          <div style={{ display: 'flex', color: 'white', borderBottom: '1px solid black' }}>
+            <Typography variant="h6" style={{ margin: '15px' }}>
+              Confirm Delete
+            </Typography>
+          </div>
+          <div style={{ display: 'flex', color: 'white', marginBottom: '20px', borderBottom: '1px solid black' }}>
+            <Typography style={{ margin: '20px 15px' }}>Do you want to delete this product?</Typography>
+          </div>
+          <div style={{ display: 'flex', color: 'white', justifyContent: 'space-between', margin: '0px 40px' }}>
+            <Button
+              variant="contained"
+              sx={{ color: 'white', backgroundColor: '#a16eff' }}
+              onClick={() => {
+                handleRemovebtn();
+                setIsModalOpen(false);
+              }}
+            >
+              Yes
+            </Button>
+            <Button
+              variant="contained"
+              sx={{ color: 'white', backgroundColor: '#ff3d71' }}
+              onClick={() => setIsModalOpen(false)}
+            >
+              No
+            </Button>
+          </div>
+        </Box>
+      </Modal>
     </>
   );
 };
